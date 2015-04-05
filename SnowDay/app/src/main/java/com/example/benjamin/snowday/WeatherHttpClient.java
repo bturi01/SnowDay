23d7a10fd027794b2cf12ce0b4aa7f6c6686fc59
unknown <bturi01@bellarmine.edu>
2015-04-05 19:02:35 -0400

    App for background data collection; Still in progress

--- SnowDay/app/src/main/java/com/example/benjamin/snowday/WeatherHttpClient.java
+++ SnowDay/app/src/main/java/com/example/benjamin/snowday/WeatherHttpClient.java
@@ -1,14 +1,4 @@
-/**
- * This is a tutorial source code 
- * provided "as is" and without warranties.
- *
- * For any question please visit the web site
- * http://www.survivingwithandroid.com
- *
- * or write an email to
- * survivingwithandroid@gmail.com
- *
- */
+
 package com.example.benjamin.snowday;
 
 import java.io.BufferedReader;
@@ -17,21 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-/*
- * Copyright (C) 2013 Surviving with Android (http://www.survivingwithandroid.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 public class WeatherHttpClient {
 
     private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
